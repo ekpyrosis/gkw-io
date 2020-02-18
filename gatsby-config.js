082be4aea428f2ad76f4d04bc645d0a3d60e4268
ekpyrosis <ekpyrosis@gmail.com>
2020-02-18 10:41:04 +0300

    config edited

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Creative Portfolio`,
+    title: `GKW Technologies`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
