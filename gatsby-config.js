5a9ff9bf10f5816141ed9b9497cfbe17d67626e9
ekpyrosis <ekpyrosis@gmail.com>
2020-02-26 14:23:12 +0300

    ganal fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,13 +26,13 @@ module.exports = {
                 // Enables Google Optimize using your container Id
                 optimizeId: "GTM-TH7JHLQ",
                 // Enables Google Optimize Experiment ID
-                experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+                //experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
                 // Set Variation ID. 0 for original 1,2,3....
-                variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+                //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
                 // Any additional optional fields
                 sampleRate: 5,
                 siteSpeedSampleRate: 10,
-                cookieDomain: "example.com",
+                cookieDomain: "gkw.io",
             },
         },
         {
