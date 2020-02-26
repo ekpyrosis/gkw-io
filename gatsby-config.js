8727a574bf302534778281d98d01dabfeaf1bda9
ekpyrosis <ekpyrosis@gmail.com>
2020-02-26 13:57:33 +0300

    ganalytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,45 @@
 require('dotenv').config()
 
 module.exports = {
-  siteMetadata: {
-    title: `GKW Technologies`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`,
-    `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-source-datocms`,
-      options: {
-        apiToken: process.env.DATO_API_TOKEN,
-      },
+    siteMetadata: {
+        title: `GKW Technologies`,
     },
-  ],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sass`,
+        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // The property ID; the tracking code won't be generated without it
+                trackingId: "UA-206111-53",
+                // Defines where to place the tracking script - `true` in the head and `false` in the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                exclude: ["/preview/**"],
+                // Delays sending pageview hits on route update (in milliseconds)
+                pageTransitionDelay: 0,
+                // Enables Google Optimize using your container Id
+                optimizeId: "GTM-TH7JHLQ",
+                // Enables Google Optimize Experiment ID
+                experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+                // Set Variation ID. 0 for original 1,2,3....
+                variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+                // Any additional optional fields
+                sampleRate: 5,
+                siteSpeedSampleRate: 10,
+                cookieDomain: "example.com",
+            },
+        },
+        {
+            resolve: `gatsby-source-datocms`,
+            options: {
+                apiToken: process.env.DATO_API_TOKEN,
+            },
+        },
+    ],
 }
